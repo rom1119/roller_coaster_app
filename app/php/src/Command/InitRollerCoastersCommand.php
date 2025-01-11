50851c566619825d1b6d2f9f4c47d2a26d6cb572
romanpytka <rom1119@poczta.onet.pl>
2025-01-11 15:50:23 +0100

    basic implementation InitRollerCoastersCommand

--- app/php/src/Command/InitRollerCoastersCommand.php
+++ app/php/src/Command/InitRollerCoastersCommand.php
@@ -1,6 +1,9 @@
 <?php
 namespace App\Command;
 
+use App\Domain\CoasterFacade;
+use App\Domain\Model\Coaster;
+use App\Domain\Model\Wagon;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -12,17 +15,27 @@ use Symfony\Component\Console\Output\OutputInterface;
 class InitRollerCoastersCommand extends Command
 {
     public function __construct(
+        private CoasterFacade $coasterFacade
     ) {
         parent::__construct();
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        // this method must return an integer number with the "exit status code"
-        // of the command. You can also use these constants to make code more readable
+        $coaster = new Coaster();
+        $coaster->setDistance(4);
+        $coaster->setNumberOfCustomers(100);
+        $coaster->setNumberOfStaff(3);
+        $coaster->setHourFrom('8:00');
+        $coaster->setHourTo('16:00');
+        $coaster = $this->coasterFacade->addCoaster($coaster);
 
-        // return this if there was no problem running the command
-        // (it's equivalent to returning int(0))
+        $wagon = new Wagon();
+        $wagon->setNumberOfPlaces(10);
+        $wagon->setSpeed(speed: 4.5);
+        $coaster = $this->coasterFacade->addWagon($wagon, $coaster->getUuid());
+
+        $output->writeln('added one coaster w ID=' . $coaster->getUuid());
         return Command::SUCCESS;
 
 
